186d8c18d3f4d1372a8dac3c8115fe7b2b1599a0
Edgarin <maed2802@gmail.com>
2023-02-03 20:11:34 -0600

    formulas

---
+++ formulas.js
@@ -0,0 +1,16 @@
+const ladoCuadrado = 5;
+const perimetroCuadrado = ladoCuadrado * 4;
+
+const areaCuadrado = ladoCuadrado  * ladoCuadrado;
+console.log({
+    ladoCuadrado,
+    perimetroCuadrado,
+    areaCuadrado,
+
+});
+
+const ladoTriangulo1 = 6;
+const ladoTriangulo2 = 6;
+const ladoTrianguloBase = 6;
+
+
